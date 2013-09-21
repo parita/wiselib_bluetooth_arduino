95aa17c924af3a1a200f74d4a4b88786aec4eaf5
Parita Pooj <pari.pooj@gmail.com>
2013-09-21 20:21:37 +0530

    Update bluetooth_setup_master.ino

--- bluetooth_setup_master.ino
+++ bluetooth_setup_master.ino
@@ -7,17 +7,7 @@
 #define DATARATE 38400
 
 SoftwareSerial bSerial(RxD,TxD);
-/*
-String retSymb = "+RTINQ=";//start symble when there's any return
-String slaveName = ";BTSlave";//Set the Slave name ,caution that ';'must be included
-int nameIndex = 0;
-int addrIndex = 0;
- 
-String recvBuf;
-String slaveAddr;
- 
-String connectCmd = "\r\n+CONN=";
- */
+
 void terminal(){
   if(bSerial.available())
     Serial.write(bSerial.read());
@@ -47,50 +37,7 @@ void setupBlueToothConnection()
     sendBlueToothCommand("\r\n+INQ=1\r\n");
     Serial.print("Master is inquiring");
     delay(2000); // This delay is required.
-    /*
-    char recvChar;
-    while(1){
-    if(bSerial.available()){
-      recvChar = bSerial.read();
-      Serial.print(recvChar);
-      recvBuf += recvChar;
-      nameIndex = recvBuf.indexOf(slaveName);//get the position of slave name
-      //nameIndex -= 1;//decrease the ';' in front of the slave name, to get the position of the end of the slave address
-      if ( nameIndex != -1 ){
-        //Serial.print(recvBuf);
- 	addrIndex = (recvBuf.indexOf(retSymb,(nameIndex - retSymb.length()- 18) ) + retSymb.length());//get the start position of slave address	 		
- 	slaveAddr = recvBuf.substring(addrIndex, nameIndex);//get the string of slave address 			
- 	break;
-      }
-    }
-  }
-  //form the full connection command
-  connectCmd += slaveAddr;
-  connectCmd += "\r\n";
-  int connectOK = 0;
-  Serial.print("Connecting to slave:");
-  Serial.print(slaveAddr);
-  Serial.println(slaveName);
-  //connecting the slave till they are connected
-  do{
-    bSerial.print(connectCmd);//send connection command
-    recvBuf = "";
-    while(1){
-      if(bSerial.available()){
-        recvChar = bSerial.read();
- 	recvBuf += recvChar;
- 	if(recvBuf.indexOf("CONNECT:OK") != -1){
-          connectOK = 1;
- 	  Serial.println("Connected!");
- 	  bSerial.print("Connected!");
- 	  break;
- 	}else if(recvBuf.indexOf("CONNECT:FAIL") != -1){
- 	  Serial.println("Connect again!");
- 	  break;
- 	}
-      }
-    }
-  }while(0 == connectOK);*/
+   
 }
  
 void sendBlueToothCommand(char command[])
